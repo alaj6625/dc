075bef17732cae3dfb3111b7d94941b1d01331a2
Alvin Allen <alaj6625@gmail.com>
2019-02-21 02:27:59 -0600

    added database info

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+// db config
+const db = require('./config/keys').mongoURI;
 
-app.get('/', (req, res ) => res.send('Hello'));
+// connect to mongodb
+mongoose
+   .connect(db)
+   .then(() => console.log('MongoDB Connected'))
+   .catch(err => console.log(err));
+   
+app.get('/', (req, res ) => res.send('Hello World'));
 
 const port = process.env.PORT || 5000;
 
